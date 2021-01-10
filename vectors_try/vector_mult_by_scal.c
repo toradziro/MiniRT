42b6a87814ff088b500e540893cb1a0cd78632f5
Estrella Hillman <ehillman@at-m5.msk.21-school.ru>
2021-01-10 19:40:03 +0300

    vectors_first

---
+++ vectors_try/vector_mult_by_scal.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct	t_vector {
+	int x;
+	int y;
+}				s_vector;
+
+int main (int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		write(1, "Try again!\n", 11);
+		return (-1);
+	}
+	int scalar;
+	s_vector input;
+	s_vector res;
+	input.x = atoi(argv[1]);
+	input.y = atoi(argv[2]);
+	scalar = atoi(argv[3]);
+	res.x = input.x * scalar;
+	res.y = input.y * scalar;
+	printf("New vector is [%d, %d]\n", res.x, res.y);
+}
