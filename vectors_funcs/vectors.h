b4695f5d0c02223eb2c8fbf5acd105720d0bc0b3
toradziro <toradziro@gmail.com>
2021-01-21 20:06:29 +0300

    subs_adds

--- vectors_funcs/vectors.h
+++ vectors_funcs/vectors.h
@@ -25,5 +25,8 @@ typedef struct	t_normal {
 double			vector_scalar_mult(s_vector a, s_vector b);
 double			vector_length(s_vector v);
 s_normal		vector_normalise(s_vector v, double len);
+s_vector		cross_prod(s_vector a, s_vector b);
+s_vector		add_vectors(s_vector a, s_vector b);
+s_vector		subs_vectors(s_vector a, s_vector b);
 
 #endif
